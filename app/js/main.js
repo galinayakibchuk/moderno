d2c0a8800df0b80b9b79d97a7f1d565c68b329aa
galinayakibchuk <galina.yakibchuk@gmail.com>
2020-06-01 18:09:35 +0530

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -36,6 +36,16 @@ $('.icon-th-large').on('click', function(){
 });
 
 
+  
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
+
+  $('.header__btn-menu').on('click', function(){
+    $('.header__box').toggleClass('active');
+  });
+
+  
   var mixer = mixitup('.products__inner-box');
 
 
